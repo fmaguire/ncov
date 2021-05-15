6bbf5807bd3b17ff1676f2adb043ace389001cd5
Finlay Maguire <finlaymaguire@gmail.com>
2021-05-15 15:49:25 +0000

    Update canada build

--- my_profiles/canadian_builds/extract_includes.py
+++ my_profiles/canadian_builds/extract_includes.py
@@ -8,7 +8,7 @@ df = pd.read_csv(sys.argv[1], sep='\t')
 # filter to canada
 df = df[df['country'] == 'Canada']
 
-df = df[df['pangolin_lineage'].isin(['B.1.1.7', 'B.1.351', 'P.1', "P.2"])]
+df = df[df['pango_lineage'].isin(['R.1', 'B.1.617.1', 'B.1.617.2', 'B.1.617.3', "B.1.525", 'B.1.526'])]
 
 print()
 for strain in df['strain'].values:
